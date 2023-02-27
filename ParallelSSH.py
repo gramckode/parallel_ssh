70be562901420e64b8404606e19536f43582bb05
Dean Gramcko <dean.gramcko@gmail.com>
2023-02-26 16:57:54 -0800

    basic cleanup and added option to specify SSH path

--- ParallelSSH.py
+++ ParallelSSH.py
@@ -1,29 +1,56 @@
 import subprocess
 from datetime import datetime,timedelta
+from os import path
+"""
+Class to run shell commands via SSH against multiple systems in parallel
 
+Depends on OpenSSH
+
+
+Example usage:
+
+my_hostlist=['10.0.5.35','10.0.5.36', "10.0.6.15", "10.0.6.16"]
+p_ssh = ParallelSSH.ParallelSSH(max_procs=5,timeout=5,hostlist=my_hostlist)
+p_ssh.run_cmd("uptime")
+uptime_success=p_ssh.get_successes()
+p_ssh.set_timeout(15)
+p_ssh.run_cmd("ps aux")
+ps_successes=p_ssh.get_successes()
+ps_failures=p_ssh.get_failures()
+"""
 class ParallelSSH:
 
-    SSH_BIN="/usr/bin/ssh"
-
-    def __init__(self,max_procs=1,timeout=None,hostlist=None):
+    def __init__(self,max_procs=1,timeout=None,hostlist=None,ssh_bin="/usr/bin/ssh"):
+        """constructor
+        Args:
+            + max_procs (int): maximum number of SSH process to run in parallel
+            + timeout (int): time in seconds to wait before killing SSH processes
+                that have not completed. If None, then waits until SSH process exits.
+            + hostlist (list): list of strings containing hostnames or IP addresses that 
+                commands will be run against
+            + ssh_bin (string): full path to OpenSSH binary. Defaults to /usr/bin/ssh
+        """
+        self.ssh_bin=ssh_bin
         self.__check_ssh()
         self.max_procs=max_procs
         self.timeout=timeout
-        self.cmd_output=None
+        self.hostlist=hostlist
+        self.cmd_successes=None
         self.cmd_failures=None
 
 
-    def __append_failure(self,hostname,error):
-        self.cmd_failures.append((hostname,error))
+    def __append_failure(self,hostname,error,returncode):
+        self.cmd_failures.append((hostname,error,returncode))
 
-    def __append_output(self, hostname, stdout,stderr):
-        self.cmd_output.append((hostname,stdout.decode("UTF-8"),stderr.decode("UTF-8")))
+    def __append_success(self, hostname, stdout,stderr,returncode):
+        self.cmd_successes.append((hostname,stdout.decode("UTF-8"),stderr.decode("UTF-8"),returncode))
 
 
     def __check_ssh(self):
-
         """Check that available SSH implementation is compatible. Raise error if not"""
-        version_cmd=[self.SSH_BIN, "-V"]
+        if not path.isfile(self.ssh_bin):
+            raise RuntimeError(f"Expected SSH path is missing: {self.ssh_bin}")
+        version_cmd=[self.ssh_bin, "-V"]
         version_cmd_fmt=" ".join(version_cmd)
         #openssh prints version to stderr
         proc=subprocess.run(version_cmd, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
@@ -37,19 +64,32 @@ class ParallelSSH:
             raise RuntimeError(f"Expecting OpenSSH implementation:\n {version_cmd_fmt} reported {ssh_ver}.")
 
 
-    def get_output(self):
-        return self.cmd_output
+    def get_successes(self):
+        """ return results for hosts where command ran succesfully.
+        Structure is list of tuples as:
+            (<hostname>,<stdout output from host>,<stderr output from host>,<exit code>)
+        """
+        return self.cmd_successes
 
     def get_failures(self):
+        """ return results for hosts where command failed. See __append_failure for data structure
+        Structure is list of tuples:
+            (<hostname>,<error message>,<exit code (None if unavaialable)>)
+        """
         return self.cmd_failures
 
-    def run_cmd(self,remote_cmd):
+    def run_cmd(self,remote_cmd,expected_exit_code=0):
+        """Run a command against the list of hosts
+        Args:
+            +remote_cmd (str): the command to run
+            +expected_exit_code (int): Optional. Specify the return code you expect for the SSH process. Default is 0.
+        """
 
-        self.cmd_output=[]
+        self.cmd_successes=[]
         self.cmd_failures=[]
 
         #base SSH command
-        ssh_cmd=[self.SSH_BIN, "-nqo", "BatchMode=yes"]
+        ssh_cmd=[self.ssh_bin, "-nqo", "BatchMode=yes"]
         cmds_to_run=[ ssh_cmd + [host,remote_cmd] for host in self.hostlist ]
 
         #hostname expected to be 4th argument in ssh command
@@ -75,15 +115,17 @@ class ParallelSSH:
                     proc,start_time = running_procs[i]
                     if not proc.poll()==None: #process finished
                         out,err=proc.communicate()
-                        self.__append_output(get_hostname(proc),out,err)
+                        if proc.returncode==expected_exit_code:
+                            self.__append_success(get_hostname(proc),out,err,proc.returncode)
+                        else:
+                            self.__append_failure(get_hostname(proc),"Unexpected Return Code", proc.returncode)
                         running_procs[i]=None
-                        continue
                     if not self.timeout==None:
                         #check if process has exceeded timeout
                         if datetime.now() >= start_time+timedelta(seconds=self.timeout):
                             proc.kill()
                             running_procs[i]=None
-                            self.__append_failure(get_hostname(proc),"Timeout Exceeded")
+                            self.__append_failure(get_hostname(proc),"Timeout Exceeded",None)
             #remove finished processes
             running_procs=[x for x in running_procs if x is not None]
 
